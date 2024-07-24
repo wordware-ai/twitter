55205d305d1c79fe6b41ba3fd081271ca204dc41
Kamil <zowczak.kamil@gmail.com>
2024-07-24 16:47:47 +0800

    📝 (page.tsx): Improve layout and responsiveness by updating CSS classes and adding conditional rendering for elements on the page.

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -40,7 +40,7 @@ const Page = async ({ params }: { params: { username: string } }) => {
     <div className="flex-center relative min-h-screen w-full flex-col gap-12 bg-[#F9FAFB] px-4 py-28 sm:px-12 md:px-28 md:pt-24">
       <div className="flex-center fixed top-0 z-50 w-full border-b bg-white/80 py-2 shadow-[5px_5px_30px_rgba(190,190,190,0.15),-5px_-5px_30px_rgba(255,255,255,0.15)] backdrop-blur-sm">
         <div className="flex w-full flex-col items-center justify-between gap-4 px-2 md:flex-row md:px-12">
-          <div className="flex w-full">
+          <div className="hidden w-full md:flex">
             <Button
               size={'sm'}
               variant={'outline'}
@@ -53,7 +53,7 @@ const Page = async ({ params }: { params: { username: string } }) => {
               </Link>
             </Button>
           </div>
-          <div className="flex w-full items-center justify-center gap-2">
+          <div className="flex w-full items-center justify-center gap-2 whitespace-nowrap">
             This agent has been built with
             <a
               href="https://wordware.ai/"
@@ -64,7 +64,17 @@ const Page = async ({ params }: { params: { username: string } }) => {
               />
             </a>
           </div>
-          <div className="flex w-full items-center justify-end gap-2">
+          <div className="flex w-full items-center justify-between gap-2 md:justify-end">
+            <Button
+              size={'sm'}
+              variant={'outline'}
+              asChild>
+              <Link
+                className="flex-center gap-2 md:hidden"
+                href={'/'}>
+                <PiCaretLeft />
+              </Link>
+            </Button>
             <Button
               size={'sm'}
               variant={'default'}
@@ -78,7 +88,9 @@ const Page = async ({ params }: { params: { username: string } }) => {
                   color={'white'}
                   width={12}
                 />
-                Duplicate this AI Agent
+                <p>
+                  Duplicate <span className="hidden md:inline">this</span> AI Agent
+                </p>
               </a>
             </Button>
             <Button
@@ -89,7 +101,10 @@ const Page = async ({ params }: { params: { username: string } }) => {
                 href="https://x.com/wordware_ai"
                 target="_blank"
                 className="flex-center gap-2">
-                <PiXLogo size={18} /> Follow us
+                <PiXLogo size={18} />
+                <p>
+                  Follow <span className="hidden md:inline">us</span>
+                </p>
               </a>
             </Button>
           </div>
