2edffc7ef7c3df998784d0a77a0f30a4ecc1f3d9
robertchandler <robert@heydaily.co>
2024-07-28 13:46:23 -0700

    Fix Kamil's crap code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Suspense } from 'react'
+import {Suspense} from 'react'
 import Link from 'next/link'
-import { PiGithubLogo } from 'react-icons/pi'
+import {PiGithubLogo} from 'react-icons/pi'
 
 import Quote from '@/app/quote'
 import WordwareLogo from '@/components/logo'
 import NewUsernameForm from '@/components/new-username-form'
-import { Button } from '@/components/ui/button'
+import {Button} from '@/components/ui/button'
 
 import TopList from './top-list'
 
@@ -15,7 +15,8 @@ const Page = () => {
   return (
     <section>
       <div className="flex flex-col md:flex-row">
-        <div className="relative flex min-h-screen flex-col justify-center bg-[#F9FAFB] p-8 sm:p-12 md:w-1/2 md:p-20 lg:p-24">
+        <div
+          className="relative flex min-h-screen flex-col justify-center bg-[#F9FAFB] p-8 sm:p-12 md:w-1/2 md:p-20 lg:p-24">
           <a
             className="top-12 pb-4 lg:absolute"
             href="https://www.producthunt.com/posts/twitter-personality-analysis-ai-agent?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-twitter&#0045;personality&#0045;analysis&#0045;ai&#0045;agent"
@@ -23,7 +24,7 @@ const Page = () => {
             <img
               src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=474144&theme=dark"
               alt="Twitter&#0032;Personality&#0032;Analysis&#0032;&#0045;&#0032;AI&#0032;agent - AI&#0032;agent&#0032;that&#0032;analyzes&#0032;tweets&#0032;to&#0032;reveal&#0032;your&#0032;unique&#0032;traits&#0046; | Product Hunt"
-              style={{ width: '166px', height: '36px' }}
+              style={{width: '166px', height: '36px'}}
               width={166}
               height={36}
             />
@@ -31,10 +32,10 @@ const Page = () => {
 
           <div>
             <h1 className="mb-8 text-4xl md:text-5xl 2xl:text-6xl">
-              discover your <br />
+              discover your <br/>
               <span
                 className="bg-clip-text text-transparent"
-                style={{ backgroundColor: '#CB9F9F' }}>
+                style={{backgroundColor: '#CB9F9F'}}>
                 {' '}
                 twitter{' '}
               </span>
@@ -43,18 +44,18 @@ const Page = () => {
 
             <div className="mb-8 flex items-center pt-2">
               <Suspense>
-                <NewUsernameForm />
+                <NewUsernameForm/>
               </Suspense>
             </div>
 
             <p className="mb-8 pt-8 text-base 2xl:text-lg">
               we built an AI agent that analyzes your tweets
-              <br />
+              <br/>
               <span className="font-medium">to reveal the unique traits that make you, you.</span>
-              <br />
-              <br />
+              <br/>
+              <br/>
               plus, we provide you with interesting insights about
-              <br />
+              <br/>
               your love life, goals, or how others perceive you.
             </p>
           </div>
@@ -62,11 +63,11 @@ const Page = () => {
           <div className="bottom-6 space-y-3 border-t xl:absolute">
             <p className="pt-3 text-sm">
               this app is powered by Wordware - an IDE for building AI agents.
-              <br />
+              <br/>
               you can build your own AI app in ~15 minutes.{' '}
               <span
                 className="bg-clip-text font-bold text-transparent"
-                style={{ backgroundColor: '#CB9F9F' }}>
+                style={{backgroundColor: '#CB9F9F'}}>
                 <Link
                   href="https://www.wordware.ai/"
                   target="_blank"
@@ -100,27 +101,26 @@ const Page = () => {
                   href="https://github.com/wordware-ai/twitter"
                   target="_blank"
                   className="flex-center gap-2">
-                  <PiGithubLogo />
+                  <PiGithubLogo/>
                   GitHub Repo
                 </a>
               </Button>
             </div>
-            <div className="flex flex-col gap-6"> 
-              <div className="flex flex-col gap-2">
-                <p className="text-sm mt-2">
+            <div className="flex flex-col gap-2">
+              <p className="text-sm mt-8">
                   <span
                     className="bg-clip-text font-bold text-transparent"
-                    style={{ backgroundColor: '#CB9F9F' }}>
+                    style={{backgroundColor: '#CB9F9F'}}>
                     support the Wordware launch this Friday!
                   </span>
-                </p>
+              </p>
               <a
                 href="https://www.producthunt.com/posts/wordware-yc-s24?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-wordware&#0045;yc&#0045;s24"
                 target="_blank">
                 <img
                   src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=474117&theme=dark"
                   alt="Wordware&#0032;&#0040;YC&#0032;S24&#0041; - web&#0045;hosted&#0032;IDE&#0032;for&#0032;building&#0032;AI&#0032;agents | Product Hunt"
-                  style={{ width: '166px', height: '36px' }}
+                  style={{width: '166px', height: '36px'}}
                   width={166}
                   height={36}
                 />
@@ -128,14 +128,13 @@ const Page = () => {
             </div>
           </div>
         </div>
-
         <div className="flex h-full w-full items-center justify-center bg-[#F6F0F0] md:h-auto md:w-1/2">
           <div className="hidden md:block">
-            <Quote />
+            <Quote/>
           </div>
         </div>
       </div>
-      <TopList />
+      <TopList/>
     </section>
   )
 }
