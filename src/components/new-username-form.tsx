c1c3468102fe3eaf3eb6d4a96563c64be8fe9a1f
robertchandler <robert@heydaily.co>
2024-08-01 17:30:26 -0700

    Revert "disable twitter"
    This reverts commit 9fe8e5d75c055fa110c147d77f468224dbabee11.

--- src/components/new-username-form.tsx
+++ src/components/new-username-form.tsx
@@ -40,28 +40,26 @@ const NewUsernameForm = () => {
    * @param {z.infer<typeof formSchema>} values - Form values
    */
   async function onSubmit(values: z.infer<typeof formSchema>) {
-    //
-    window.location.href = 'https://tally.so/r/3lRoOp'
-    // const cleanedUsername = cleanUsername(values.username)
-    // let response = await handleNewUsername({ username: cleanedUsername })
-    // console.log('🟣 | file: new-username-form.tsx:46 | onSubmit | response:', response)
-    // if (response?.error) {
-    //   window.location.href = 'https://tally.so/r/3lRoOp'
-    // }
+    const cleanedUsername = cleanUsername(values.username)
+    let response = await handleNewUsername({ username: cleanedUsername })
+    console.log('🟣 | file: new-username-form.tsx:46 | onSubmit | response:', response)
+    if (response?.error) {
+      window.location.href = 'https://tally.so/r/3lRoOp'
+    }
 
-    // const start = Date.now()
-    // while (response && !response?.found && Date.now() - start < 100000) {
-    //   await new Promise((resolve) => setTimeout(resolve, 5000))
-    //   response = await handleNewUsername({ username: cleanedUsername })
-    //   if (response?.error) {
-    //     window.location.href = 'https://tally.so/r/3lRoOp'
-    //   }
-    // }
+    const start = Date.now()
+    while (response && !response?.found && Date.now() - start < 100000) {
+      await new Promise((resolve) => setTimeout(resolve, 5000))
+      response = await handleNewUsername({ username: cleanedUsername })
+      if (response?.error) {
+        window.location.href = 'https://tally.so/r/3lRoOp'
+      }
+    }
 
-    // // We didn't get results in after the timeout, redirect
-    // if (Date.now() - start >= 100000) {
-    //   window.location.href = 'https://tally.so/r/3lRoOp'
-    // }
+    // We didn't get results in after the timeout, redirect
+    if (Date.now() - start >= 100000) {
+      window.location.href = 'https://tally.so/r/3lRoOp'
+    }
   }
 
   return (
