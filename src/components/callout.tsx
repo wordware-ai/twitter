07d134ca1580e5bc36c37cd7e4c39f5da3e78545
Kamil <zowczak.kamil@gmail.com>
2024-08-02 13:49:33 +0700

    fix hide button

--- src/components/callout.tsx
+++ src/components/callout.tsx
@@ -1,7 +1,5 @@
 // import { PiGithubLogo } from 'react-icons/pi'
 
-import { PiGithubLogo } from 'react-icons/pi'
-
 import { Button } from '@/components/ui/button'
 
 import WordwareLogo from './logo'
