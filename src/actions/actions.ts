bd66fa120c6e7b6fa9ce50074238bbfd8e9666eb
Kamil <zowczak.kamil@gmail.com>
2024-08-06 00:43:03 +0700

    change cache key

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -423,6 +423,6 @@ export const getStatistics = cache(
     const lastTimestamp = lastElement ? new Date(lastElement.timestamp) : new Date()
     return { chartData: formattedResult.slice(0, -1), timestamp: lastTimestamp.toISOString() }
   },
-  ['statistics'],
+  ['statistics-posthog'],
   { revalidate: 3600 }, // Cache for 1 hour (3600 seconds)
 )
