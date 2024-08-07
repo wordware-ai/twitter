264369fd004d1336859571d491db467aa1126a4c
Kamil <zowczak.kamil@gmail.com>
2024-08-07 15:42:53 +0700

    timeout increase

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/ingest/static/:path*',
-                destination: 'https://us-assets.i.posthog.com/static/:path*',
-            },
-            {
-                source: '/ingest/:path*',
-                destination: 'https://us.i.posthog.com/:path*',
-            },
-        ]
-    },
-    // This is required to support PostHog trailing slash API requests
-    skipTrailingSlashRedirect: true,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'pbs.twimg.com',
-                pathname: '**',
-            },
-        ],
-    },
-    typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-    },
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+    ]
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+        pathname: '**',
+      },
+    ],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  staticPageGenerationTimeout: 120,
 }
 
 export default nextConfig
